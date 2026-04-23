c9a98553b31f5e50278c776ee76b84d9712750b5
yuu200219 <w200219200219@gmail.com>
2026-04-23 20:52:13 +0800

    update malloc.c

--- ch03/malloc.c
+++ ch03/malloc.c
@@ -9,7 +9,7 @@ int main() {
     char* p2;
 	char command[1024];
     /*印出行程的pid，方便我們到/proc目錄裡面找到相對映的檔案*/
-    //printf("pid = %d\n", getpid());
+    printf("pid = %d\n", getpid());
     printf("malloc(64)\n");
     /*配置64byte記憶體*/
 	p1 = (char*)malloc(64);
